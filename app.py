78f407fce0dc5c3d2b114622c3805f0829cbda00
mahd18-meet <mahdqumseya@gmail.com>
2018-01-18 18:44:20 +0200

    Fixed it to be like my website

--- app.py
+++ app.py
@@ -1,32 +1,89 @@
 # flask imports
 from flask import Flask, Response, render_template, request, redirect, url_for
+import uuid
+
+from flask_sqlalchemy import SQLAlchemy
 
 # flask setup
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ITSASECRET"
+db=SQLAlchemy(app)
 
 # flask-login imports
 from flask_login import LoginManager
 from flask_login import login_required, current_user 
-from login import login_manager, login_handler, logout_handler
-login_manager.init_app(app)
+
+
+from create_db import Links, Base
 
 # SQLAlchemy
 
-from model import Base,User,Post
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///project.db')
+engine = create_engine('sqlite:///./project.db')
 Base.metadata.bind = engine
+Base.metadata.create_all(bind=engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-@app.route('/')
-def hello_world():
-    posts=session.query(Post).order_by("id desc").all()  
-    return render_template('home.html')
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.upper() # Make all characters uppercase.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length] # Return the random strin
+
+
+
+def random_letters():
+	shortened_link = ""
+	if request.form['spl'] == "":
+		
+		shortened_link= my_random_string()
+		
+	else:
+		shortened_link = request.form['spl']
+
+	return shortened_link
+
+
+
+@app.route('/' , methods=['GET', 'POST'])
+def home():
+
+	if request.method == "GET":
+		#show the webpage
+		print "This is GET"
+		return render_template('home.html')	
+	elif request.method=="POST":
+		print "This is post"
+
+		
+
+		
+		link= Links(request.form['user'],request.form['original_link'],random_letters())
+		db.session.add(link)
+
+		
+		
+		db.session.commit()
+		print "committed"
+		
+		
+		
+
+		
+		return redirect(url_for('home'))
+
+
+
+
+
+		
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -41,64 +98,10 @@ def logout():
 @app.route('/protected', methods=["GET"])
 @login_required
 def protected():
+
     return render_template('protected.html')
 
 
-@app.route('/Category/Boys')
-def Clothes():
-	posts=session.query(Post).filter_by(category="Clothes").all()
-	return render_template('lifehacxcat.html' ,posts=posts)
-
-@app.route('/Category/Girls')
-def Garden():
-	posts=session.query(Post).filter_by(category="Garden").all()
-	return render_template('lifehacxcat.html',posts=posts)
-
-@app.route('/Category/Both')
-def Food():
-	posts=session.query(Post).filter_by(category="Food").all()
-	return render_template('lifehacxcat.html', posts=posts)
-
-@app.route('/Category/Young')
-def Makeup():
-	posts=session.query(Post).filter_by(category="Makeup").all()
-	return render_template('lifehacxcat.html', posts=posts)
-
-@app.route('/Category/Old')
-def Home_Design():
-	posts=session.query(Post).filter_by(category="Home_Design").all()
-	return render_template('lifehacxcat.html',posts=posts)
-
-@app.route('/Category/Others')
-def Camping():
-	posts=session.query(Post).filter_by(category="Camping").all()
-	return render_template('lifehacxcat.html',posts=posts)
-
-@app.route('/Category/Stories')
-def Other():
-	posts=session.query(Post).filter_by(category="Other").all()
-	return render_template('lifehacxcat.html', posts=posts)
-
-@app.route('/Add_Hack', methods=["GET", "POST"])
-def Add_Hack():
-	if request.method == "GET":
-		#show the webpage
-		return render_template('Add_Hack.html')	
-	if request.method == "POST":
-		vid_url = request.form.get("vid_url")
-		title = request.form.get("title")
-		category = request.form.get("category")
-		description = request.form.get("description")
-
-		embed_video = vid_url
-		
-		vid_url = 'https://youtube.com/embed/'+ embed_video.split('=')[1]
-
-		new_post = Post(title=title, description=description,
-			video_url=vid_url,category=category)
-		session.add(new_post)
-		session.commit()
-		return redirect(url_for('hello_world'))
 
 		# redirect
 @app.route('/', methods=["GET","POST"])
@@ -147,20 +150,4 @@ def login_form():
 app.run(debug=True)
 
 
-'''
-	THINGS IN A POST
 
-	title = Column(Text (50))
- 	description = Column(String(1000))
- 	video_url = Column(String(500))
- 	category = Column(String(20))
-
-
- 	BACKEND HTML
-
-	title
-	category
-	vid_url
-	description
-
- 	'''
--- create_db.py
+++ create_db.py
@@ -1,8 +1,28 @@
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
 
-engine = create_engine("postgres://localhost/mydb")
-if not database_exists(engine.url):
-    create_database(engine.url)
+from flask_sqlalchemy import SQLAlchemy
 
-print(database_exists(engine.url))
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, Text
+
+
+
+
+
+class Links(Base):
+	__tablename__="links"
+	id = Column(Integer, primary_key=True)
+	user = Column(String(120),unique =False)
+	original_link = Column(String(350),unique =False)
+	shortened_link = Column(String(350),unique =True)
+
+
+	def __init__(self,user,original_link,shortened_link):
+		self.user = user
+		self.original_link = original_link
+		self.shortened_link =shortened_link
+
+
+	def __repr__(self):
+		return self.user, self.original_link, self.shortened_link
+
